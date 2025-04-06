a7104618c3826e82b46e4f5d9ae985db686e5bf7
paulbqnt <68791915+paulbqnt@users.noreply.github.com>
2025-04-06 10:02:13 +0200

    fix/feat: make sure backend consider only the sqlite.db in root ; clean uv config

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,4 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -5,15 +6,21 @@ from sqlalchemy.orm import sessionmaker
 def get_database_url(db_path="sqlite.db"):
     """
     Function to get the database URL.
-    You can modify this function to return different database URLs based on your needs.
+    Constructs the path to the database file relative to the project root.
     """
-    return f"sqlite:///{db_path}"
+    # Get the directory of the current file
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
+    # Construct the path to the root of the project
+    project_root = os.path.abspath(os.path.join(current_dir, '..', '..'))
 
+    # Construct the full path to the database file
+    db_full_path = os.path.join(project_root, db_path)
+
+    return f"sqlite:///{db_full_path}"
 
 # SQLite database URL
 SQLALCHEMY_DATABASE_URL = get_database_url()
-
 
 # Create engine
 engine = create_engine(
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.app.pricer.router import router as pricer_router
